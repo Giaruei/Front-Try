9ef0dc2b1ff9573d94a00e676ecd777ac26889c5
giaruei <caigiaruei@gmail.com>
2024-11-17 13:55:18 +0800

    修改调用函数

--- promise.js
+++ promise.js
@@ -175,7 +175,7 @@ const p1 = Promise.resolve(1);
 const p2 = Promise.resolve(2);
 const p3 = Promise.resolve(3);
 
-Promise.all([p1, p2, p3])
+promiseAll([p1, p2, p3])
   .then((res) => console.log(res)) // [1, 2, 3]
   .catch((e) => console.log(e));
 
